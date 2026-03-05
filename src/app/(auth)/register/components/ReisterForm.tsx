4d462a95366ed7154880f4e1894491f5149d269b
EmanRadwan114 <emanalaa11499@gmail.com>
2026-03-06 00:32:23 +0200

    refactor: handle signup logic in custom hook

--- src/app/(auth)/register/components/ReisterForm.tsx
+++ src/app/(auth)/register/components/ReisterForm.tsx
@@ -11,30 +11,77 @@ import {
 } from "../validation/register.validation";
 import PasswordField from "@/components/PasswordField";
 import Spinner from "@/components/Spinner";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/lib/firebase/firebase.config";
+import { toast } from "react-toastify";
+import { updateProfile, User } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 const ReisterForm: React.FC = () => {
+  const router = useRouter();
+
   // react hook form & zod
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors, isSubmitting, isValid },
   } = useForm<RegisterInput>({
     resolver: zodResolver(registerSchema),
     mode: "onChange",
   });
 
-  const onSubmit = (data: RegisterInput) => {
-    console.log(data);
+  // firbase
+  const [createUserWithEmailAndPassword, , , error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const firebaseErr =
+    (
+      error?.customData as unknown as {
+        _tokenResponse: {
+          error: {
+            message: string;
+          };
+        };
+      }
+    )?._tokenResponse?.error?.message || error?.message;
+
+  // handlers
+  const onSubmit = async (data: RegisterInput) => {
+    try {
+      const res = await createUserWithEmailAndPassword(
+        data.email,
+        data.password,
+      );
+
+      // update profile to store username
+      if (res?.user) {
+        await updateProfile(res?.user as User, {
+          displayName: data.fullName,
+        });
+        toast.success("Account created successfully");
+        reset();
+        router.push("/login");
+      }
+    } catch (error: unknown) {
+      toast.error((error as { message: string })?.message);
+    }
   };
 
   return (
     <section className="p-6 bg-white shadow-lg rounded-md sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-1/3">
+      {/* header */}
       <header className="text-center space-y-2 mb-4">
         <h1 className="text-2xl font-bold">Create Account</h1>
         <p className="text-gray-600">
           Please fill in the form below to create an account
         </p>
+
+        {/* firebase error */}
+        {error && <p className="text-red-800 text-sm">{firebaseErr}</p>}
       </header>
+
+      {/* form */}
       <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
         <FormField
           label="Full Name"
@@ -75,7 +122,7 @@ const ReisterForm: React.FC = () => {
         </Button>
       </form>
 
-      {/* login */}
+      {/* login link */}
       <p className="text-center mt-4 text-gray-600">
         Already have an account?{" "}
         <Link

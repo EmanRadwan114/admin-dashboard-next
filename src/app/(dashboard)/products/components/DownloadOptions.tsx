cd5a85eb5cc3ba93b2638b4b8920e7a84357d87b
EmanRadwan114 <emanalaa11499@gmail.com>
2026-03-08 08:06:46 +0200

    feat: handle export file logic

--- src/app/(dashboard)/products/components/DownloadOptions.tsx
+++ src/app/(dashboard)/products/components/DownloadOptions.tsx
@@ -1,13 +1,64 @@
 "use client";
 import React, { useState } from "react";
+import { utils, writeFile } from "xlsx";
+import { IProduct } from "../types/products.types";
+import { useAppSelector } from "@/lib/redux-toolkit/hooks";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { tableHeaders } from "../data/tableHeaders";
 
 const DownloadOptions: React.FC = () => {
   const [option, setOption] = useState("");
+  const { cachedProducts } = useAppSelector((state) => state.products);
 
+  const exportAsXlsxFile = (data: IProduct[]) => {
+    const workSheet = utils.json_to_sheet(data);
+    const workBook = utils.book_new();
+    utils.book_append_sheet(workBook, workSheet, "Data");
+    writeFile(workBook, "products.xlsx");
+  };
+
+  const exportAsPdfFile = (data: IProduct[]) => {
+    const doc = new jsPDF();
+    doc.text("Products Data", 10, 10);
+
+    autoTable(doc, {
+      head: [
+        [
+          ...tableHeaders.filter((h) => h.toLocaleLowerCase() !== "image"),
+          "Discount Price",
+        ],
+      ],
+      body: data.map((item) => [
+        item.title,
+        item.description,
+        item.category,
+        item.availabilityStatus,
+        `${item.price}$`,
+        `${item.discountPercentage}% off`,
+      ]),
+      styles: {
+        cellPadding: 2,
+        fontSize: 8,
+        valign: "middle",
+        minCellHeight: 10,
+      },
+    });
+
+    doc.save("products.pdf");
+  };
+
+  //   handlers
   const handleOptionChange = (
     e: React.ChangeEvent<HTMLSelectElement, HTMLSelectElement>,
   ) => {
     setOption(e.target.value);
+    if (e.target.value === "xlsx") {
+      exportAsXlsxFile(cachedProducts);
+    } else if (e.target.value === "pdf") {
+      exportAsPdfFile(cachedProducts);
+    }
+    setOption("");
   };
 
   return (
@@ -17,7 +68,7 @@ const DownloadOptions: React.FC = () => {
       className="outline outline-gray-500 rounded-md p-2 focus-within:ring-2 focus-within:ring-foreground focus-within:outline-offset-4 w-full"
     >
       <option value="" disabled>
-        -- Choose Download Formate --
+        -- Choose Export Formate --
       </option>
       <option value="pdf">PDF</option>
       <option value="xlsx">XLSX</option>
